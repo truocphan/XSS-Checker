acbf8c43f645f9de93cf2a5209ec6e3caafd0c85
Truoc Phan <57470560+truocphan@users.noreply.github.com>
2022-06-29 03:26:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,27 @@ import setuptools
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
+with open('requirements.txt') as f:
+	requirements = f.readlines()
+
 setuptools.setup(
-	name="XSS-Checker",  
-	version="22.6.28.1",
-	author="Truoc Phan",
-	scripts=["XSS-Checker.py"],
-	license="MIT",
-	author_email="truocphan112017@gmail.com",
-	description="XSS-Checker is a tool used to Check / Validate for XSS vulnerabilities",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	install_requires=[
-		"colored",
-		"requests",
-		"selenium",
-		"webdriver_manager"
-	],
-	url="https://github.com/truocphan/XSS-Checker",
-	packages=setuptools.find_packages(),
-	classifiers=[
+	name = "XSS-Checker",  
+	version = "22.6.29",
+	author = "Truoc Phan",
+	license = "MIT",
+	author_email = "truocphan112017@gmail.com",
+	description = "XSS-Checker is a tool used to Check / Validate for XSS vulnerabilities",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	install_requires = requirements,
+	url = "https://github.com/truocphan/XSS-Checker",
+	packages = setuptools.find_packages(),
+	classifiers = [
 		"Programming Language :: Python :: 3"
 	],
+	entry_points = {
+		"console_scripts": [
+			"XSS-Checker = XSS_Checker:vFaT3_jrF1FN30CA_o46KQ432mUyxGsUjjQqkXNFQzO0iHoX"
+		]
+	},
 )
